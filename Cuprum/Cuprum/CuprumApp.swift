856ae65704653c47510dfded822c68aaf28def54
Kyvnkyr <yqhkevin2008@icloud.com>
2022-11-23 12:52:22 +0800

    Cuprum Terminal Project

---
+++ Cuprum/Cuprum/CuprumApp.swift
@@ -0,0 +1,17 @@
+//
+//  CuprumApp.swift
+//  Cuprum
+//
+//  Created by KeviarCider on 23/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct CuprumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
